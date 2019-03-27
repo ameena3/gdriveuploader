b454db8133c36105f836ea9030cae40be2d1d69b
ameena3 <anubhav.meena1991@gmail.com>
2019-03-26 22:42:36 -0500

    made the driveservice

--- uploader.go
+++ uploader.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"sync"
 
 	"golang.org/x/net/context"
@@ -94,95 +93,17 @@ func main() {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 	client := getClient(config)
-	srv, err := (*driveService.FileService).GetInstance(client)
+
+	// Create the variable and then call the instance.
+	var fs driveService.FileService
+	_, err = fs.Initialize(client)
+	if err != nil {
+		log.Panicf("failed toinitialize the client the error is : %v", err)
+	}
 	if err != nil {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
-	r, err := srv.Files.List().Fields("nextPageToken, files(id, name)").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
-	}
-	fmt.Printf("Length of the files are:%v \n", r.NextPageToken)
-	var filenames []string
-	err = filepath.Walk("/home/anubhav/Projects/backup/MoviesShows/",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !info.IsDir() {
-
-				filenames = append(filenames, path)
-			}
-
-			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	}
-	//uploadFile(srv, "1conPh8WTgBqzn5BSHJAWrmuv2Sv4cRg8", "/home/anubhav/Projects/backup/MoviesShows/Silicon Valley - Season 1 - 720p BluRay - x265 HEVC - ShAaNiG/Silicon.Valley.S01E01.720p.BluRay.x265.ShAaNiG.mkv")
-	//uploadFileBatched(srv, "1Dhg2f9vtrG9PPsKvugNoTvq2vckXamjx", 5, &filenames)
-	fmt.Println("Files:")
-repeat:
-	if len(r.Files) == 0 {
-		fmt.Println("No files found.")
-	} else {
-		for _, i := range r.Files {
-
-			fmt.Printf("name is: %s The ID is: %s\n ", i.Name, i.Id)
-			if len(i.Name) > 5 {
-				if i.Name[:5] == "/home" {
-					fmt.Printf("Deleting file %v \n", i.Name)
-					err := srv.Files.Delete(i.Id).Do()
-					fmt.Println("Deleted file")
-					if err != nil {
-						log.Fatalf("cannot delete file the error is: %v\n", err)
-					}
-				}
-			}
-
-		}
-	}
-	if r.NextPageToken != "" {
-		r, err = srv.Files.List().PageToken(r.NextPageToken).Do()
-		goto repeat
-	}
-}
-
-func deleteFileBatched(srv *drive.Service, prefix string, batchSize int) {
-	panic("not implemented")
-
-}
-
-func uploadFileBatched(srv *drive.Service, parentID string, batchSize int, fileNames *[]string) {
-	for i, file := range *fileNames {
-		wg.Add(1)
-		go uploadFile(srv, parentID, file)
-		if (i)%batchSize == 0 {
-			wg.Wait()
-		}
-	}
-
-	wg.Wait()
-}
-
-func uploadFile(srv *drive.Service, parentID string, fileName string) {
-
-	up, err := os.Open(fileName)
-	if err != nil {
-		log.Printf("Unable to read file: %v", err)
-	}
-	defer up.Close()
-	ssz, _ := up.Stat()
-	fmt.Printf("uploading file %v , Size is  %v \n", ssz.Name(), ssz.Size())
-	file := drive.File{Name: up.Name(), Parents: []string{parentID}}
-
-	_, err = srv.Files.Create(&file).ResumableMedia(nil, up, ssz.Size(), "").ProgressUpdater(call).Do()
-	if err != nil {
-		log.Fatalf("Unable to upload file: %v", err)
-	}
-	wg.Done()
-}
-func call(current, total int64) {
-	fmt.Printf("Percent uploaded: %v \n", ((current * 100) / total))
+	//fs.UploadFileBatched("1Dhg2f9vtrG9PPsKvugNoTvq2vckXamjx", 5, "/home/anubhav/Projects/backup/MoviesShows/")
+	fs.DeleteFileBatched("/home", 1)
 }
